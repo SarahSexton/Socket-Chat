2cc0519b7d0ae4512d36185d0c8b361f72d29a1b
SarahSexton <sarahjeannesexton@live.com>
2015-08-08 17:46:46 -0500

    Tim helped

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+// IMPORTANT LINE
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+﻿var socket = io();
+  $('form').submit(function(){
+	socket.emit('chat message', $('#message-box').val());
+	$('#message-box').val('');
+	return false;
+  });
+  socket.on('chat message', function(msg){
+	$('#messages').append($('<p>').text(msg));
+  });
+
+
